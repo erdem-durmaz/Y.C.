5c7049e3003c796379156a794ead90dc060d7a44
erdemdurr <berdushwile@gmail.com>
2021-01-17 16:44:04 +0300

    minor

--- mysite/yaratici/views.py
+++ mysite/yaratici/views.py
@@ -11,23 +11,23 @@ def landing_page(request):
     return render(request, 'yaratici/landing.html')
 
 def cerez(request):
-    posts = BlogPost.objects.all().exclude(id=1).order_by('-create_date')
+    posts = BlogPost.objects.exclude(id=1).filter(is_Published__exact=True).order_by('-create_date')
     return render(request, 'yaratici/cerez.html', {'posts': posts})
 
 def home(request):
-    posts = BlogPost.objects.order_by('-create_date')[:3]
+    posts = BlogPost.objects.exclude(id=1).filter(is_Published__exact=True).order_by('-create_date')[:3]
     print(posts)
     kimim = BlogPost.objects.get(slug__iexact="ben-kimim")
     return render(request, 'yaratici/home.html', {'kimim': kimim, 'posts': posts})
 
 
 def posts(request):
-    posts = BlogPost.objects.all().exclude(id=1).order_by('-create_date')
+    posts = BlogPost.objects.exclude(id=1).filter(is_Published__exact=True).order_by('-create_date')
     return render(request, 'yaratici/posts.html', {'posts': posts})
 
 
 def show_post(request, slug):
-    posts = BlogPost.objects.all().exclude(id=1).order_by('-create_date')[:10]
+    posts = BlogPost.objects.exclude(id=1).filter(is_Published__exact=True).order_by('-create_date')[:10]
     post = get_object_or_404(BlogPost, slug=slug)
     return render(request, 'yaratici/post.html', {'post': post, 'posts': posts})
 
